c1674951a260fb80db02f272b67ee3f226c6cc17
Henning Odén <henning.oden@outlook.com>
2020-11-18 17:55:48 +0100

    364. REST APIs, Clients & CORS Errors

--- routes/feed.js
+++ routes/feed.js
@@ -1,11 +1,11 @@
-const express = require("express");
+const express = require('express');
 
 const router = express.Router();
 
-const feedsController = require("../controllers/feed");
+const feedsController = require('../controllers/feed');
 
 // GET /feed/posts
-router.get("/posts", feedsController.getPosts);
+router.get('/posts', feedsController.getPosts);
 
 router.post('/post', feedsController.createPost);
 
