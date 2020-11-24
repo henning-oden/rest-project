d370ed34d25805d7d5840553b8579ddff6496e42
Henning Odén <henning.oden@outlook.com>
2020-11-24 17:48:04 +0100

    384. Adding a User Model

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const { v4: uuidv4 } = require('uuid');
 const DATABASE_URI = require('./util/database');
 
 const feedRoutes = require('./routes/feed');
+const authRoutes = require('./routes/auth');
 
 const app = express();
 
@@ -53,6 +54,7 @@ app.use((req, res, next) => {
 });
 
 app.use('/feed', feedRoutes);
+app.use('/auth', authRoutes);
 
 app.use((error, req, res, next) => {
   console.log(error);
