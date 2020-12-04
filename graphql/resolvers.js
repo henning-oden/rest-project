e9abfc61962eff71e2c203eeff6a1d4105c6146d
Henning Odén <henning.oden@outlook.com>
2020-12-04 18:27:19 +0100

    248. Adding a CreatePost Mutation

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -3,16 +3,20 @@ const validator = require('validator');
 const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
+const Post = require('../models/post');
 
 module.exports = {
   createUser: async function ({ userInput }, req) {
     //   const email = args.userInput.email;
-    const errors = []
-    if(!validator.isEmail(userInput.email)) {
-      errors.push({message:'Email is invalid.'});
+    const errors = [];
+    if (!validator.isEmail(userInput.email)) {
+      errors.push({ message: 'Email is invalid.' });
     }
-    if (validator.isEmpty(userInput.password) || !validator.isLength(userInput.password, {min: 5})) {
-      errors.push({message: 'Password too short!'});
+    if (
+      validator.isEmpty(userInput.password) ||
+      !validator.isLength(userInput.password, { min: 5 })
+    ) {
+      errors.push({ message: 'Password too short!' });
     }
     if (errors.length > 0) {
       const error = new Error('Invalid input.');
@@ -34,8 +38,8 @@ module.exports = {
     const createdUser = await user.save();
     return { ...createdUser._doc, _id: createdUser._id.toString() };
   },
-  login: async function({ email, password }) {
-    const user = await User.findOne({email: email});
+  login: async function ({ email, password }) {
+    const user = await User.findOne({ email: email });
     if (!user) {
       const error = new Error('User not found.');
       error.code = 401;
@@ -47,12 +51,48 @@ module.exports = {
       error.code = 401;
       throw error;
     }
-    const token = jwt.sign({
-      userId: user._id.toString(),
-      email: user.email
-    }, 'somesupersecretsecret',
-    { expiresIn: '1h'}
+    const token = jwt.sign(
+      {
+        userId: user._id.toString(),
+        email: user.email,
+      },
+      'somesupersecretsecret',
+      { expiresIn: '1h' }
     );
     return { token: token, userId: user._id.toString() };
-  }
+  },
+  createPost: async function ({ postInput }, req) {
+    const errors = [];
+    if (
+      validator.isEmpty(postInput.title) ||
+      !validator.isLength(postInput.title, { min: 5 })
+    ) {
+      errors.push({ message: 'Title is invalid.' });
+    }
+    if (
+      validator.isEmpty(postInput.content) ||
+      !validator.isLength(postInput.content, { min: 5 })
+    ) {
+      errors.push({ message: 'Content is invalid.' });
+    }
+    if (errors.length > 0) {
+      const error = new Error('Invalid input.');
+      error.data = errors;
+      error.code = 422;
+      throw error;
+    }
+    const post = new Post({
+      title: postInput.title,
+      content: postInput.content,
+      imageUrl: postInput.imageUrl,
+    });
+    const createdPost = await post.save();
+    // add post to user's posts
+    return {
+      ...createdPost._doc,
+      _id: createdPost._id.toString(),
+      createdAt: createdPost.createdAt.toISOString(),
+      updatedAt: createdPost.updatedAt.toISOString(),
+    };
+  },
 };
--- graphql/schema.js
+++ graphql/schema.js
@@ -25,6 +25,12 @@ module.exports = buildSchema(`
         userId: String!
     }
 
+    input PostInputData {
+        title: String!
+        content: String!
+        imageUrl: String!
+    }
+
     input UserInputData {
         email: String!
         name: String!
@@ -37,6 +43,7 @@ module.exports = buildSchema(`
 
     type RootMutation {
         createUser(userInput: UserInputData): User!
+        createPost(postInput: PostInputData): Post!
     }
 
     schema {
