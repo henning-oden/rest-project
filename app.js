0d40e5c2fe32ce92d43e46889b1f129088dce8fb
Henning Odén <henning.oden@outlook.com>
2020-12-06 11:25:35 +0100

    435. Uploading Images

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
 const multer = require('multer');
 const { v4: uuidv4 } = require('uuid');
 const graphqlHttp = require('express-graphql');
+const fs = require('fs');
 
 const graphqlSchema = require('./graphql/schema');
 const graphqlResolver = require('./graphql/resolvers');
@@ -59,6 +60,21 @@ app.use((req, res, next) => {
 
 app.use(auth);
 
+app.put('/post-image', (req, res, next) => {
+  if (!req.isAuth) {
+    throw new Error('Not authenticated!');
+  }
+  if (!req.file) {
+    return res.status(200).json({ message: 'No file provided!' });
+  }
+  if (req.body.oldPath) {
+    clearImage(req.body.oldPath);
+  }
+  return res
+    .status(201)
+    .json({ message: 'File stored.', filePath: req.file.path.replace('\\', '/') });
+})
+
 app.use(
   '/graphql',
   graphqlHttp({
@@ -93,3 +109,9 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
+  const clearImage = (filePath) => {
+    filePath = path.join(__dirname, '..', filePath);
+    fs.unlink(filePath, (err) => console.log(err));
+  };
+  
